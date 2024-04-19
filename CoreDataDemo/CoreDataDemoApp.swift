563227b5a4ebf5494b71e8dd055635e85243b432
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-04-19 15:06:02 +0300

    Initial Commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Іван Штурхаль on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
